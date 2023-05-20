3f96191881db10a86df77db4e1bcc3fab01c500a
InGyu Moon <ansdlsrb1121@naver.com>
2023-05-20 15:13:55 +0900

    230520 django course 30 commit

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
+    path('profiles/', include('profileapp.urls')),
 ]
 
 
