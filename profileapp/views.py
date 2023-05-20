497015c3f210206113c51482d30877ce0d2b4f78
InGyu Moon <ansdlsrb1121@naver.com>
2023-05-20 15:52:35 +0900

    230519 django course 31 commit

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.froms import ProfileCreationForm
+from profileapp.models import Profile
+
 
 # Create your views here.
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):
+        temp_profile = form.save(commit=False) #날라온 form의 정보를 일시적으로 저장 (user 정보는 없음)
+        temp_profile.user = self.request.user #profile의 user를 request를 보낸 user로 설정
+        temp_profile.save() #해당 정보를 저장
+        return super().form_valid(form)
